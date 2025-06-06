814e7dd0a14f46e05a7b7a3d76bdc5600cca33ad
youssef mohamed <youssefhusseinnn1@gmail.com>
2025-06-06 19:05:40 +0300

    add

--- models/productDB.js
+++ models/productDB.js
@@ -26,7 +26,7 @@ const productSchema = new mongoose.Schema({
   },
   category: {
     type: String,
-    enum: ["necklace", "earings", "bracelets", "rings"],
+
     required: true,
   },
   collection: {
