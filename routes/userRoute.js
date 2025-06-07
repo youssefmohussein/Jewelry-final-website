6953b5bae9edcbdb22905278a949735d85afbc35
Norhan Khaled <norasallam742@gmail.com>
2025-06-07 16:30:58 +0300

    add

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,9 +7,7 @@ router.post("/register", userController.registerUser);
 router.post("/login", userController.loginUser);
 router.post("/resetpassword", userController.resetPassword);
 
-// User CRUD routes by Email param
-router.get("/:Email", userController.getUserByEmail);
-router.put("/:Email", userController.updateUser);
-router.delete("/:Email", userController.deleteUser);
+
+
 
 module.exports = router;
