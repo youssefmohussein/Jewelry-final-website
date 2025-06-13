3f9661fa534a6ea2b4fcbb7f518495a22d9db9e3
youssef mohamed <youssefhusseinnn1@gmail.com>
2025-06-14 01:17:04 +0300

    add

--- public/javasScript/orderManagment.js
+++ public/javasScript/orderManagment.js
@@ -172,3 +172,22 @@ function searchTable() {
     }
   });
 }
+
+async function logout() {
+  try {
+    const response = await fetch("/logout", {
+      method: "POST", // Sends a POST request
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const result = await response.json();
+    alert(result.message);
+
+    if (response.ok) {
+      window.location.href = "/"; // Redirects to the login page
+    }
+  } catch (err) {
+    console.error("Error during logout:", err);
+    alert("Something went wrong during logout.");
+  }
+}
