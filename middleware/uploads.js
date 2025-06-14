5c0b24651303273bf535844f954cf3787ef20205
youssef mohamed <youssefhusseinnn1@gmail.com>
2025-06-14 15:05:58 +0300

    ADD
    Co-Authored-By: malak_mostafa_zaki <zakimmalak90@gmail.com>
    Co-Authored-By: Norhan Khaled <175220195+norhankhaled11@users.noreply.github.com>

--- middleware/uploads.js
+++ middleware/uploads.js
@@ -1,16 +1,3 @@
-/*const multer = require("multer");
-const storage = multer.memoryStorage();
-const uploads = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 3, 
-  },
-});
-
-
-module.exports = uploads;*/
-
-
 const multer = require("multer");
 const storage = multer.memoryStorage();
 const upload = multer({
